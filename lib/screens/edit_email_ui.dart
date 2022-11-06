03058650116a19e5cf014d85d082069ed8d2052b
Thongek_Wongponom <70640558+Thongek-wpm@users.noreply.github.com>
2022-11-06 17:04:05 +0700

    06-11-2022

--- lib/screens/edit_email_ui.dart
+++ lib/screens/edit_email_ui.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EditemailUi extends StatefulWidget {
   const EditemailUi({super.key});
@@ -14,13 +16,69 @@ class _EditemailUiState extends State<EditemailUi> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+          "แก้ไขอีเมล์",
+          style: GoogleFonts.kanit(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
           },
           icon: Icon(Icons.arrow_back_ios),
         ),
+        centerTitle: true,
         backgroundColor: Colors.lightGreen,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.10,
+              ),
+              Icon(
+                Icons.email,
+                size: MediaQuery.of(context).size.width * 0.35,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.10,
+              ),
+              Text(
+                'ป้อมอีเมล์',
+                style: GoogleFonts.kanit(
+                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.width * 0.03),
+              Padding(
+                padding: const EdgeInsets.only(left: 40, right: 40),
+                child: TextField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.03,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Done"),
+                style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                  MediaQuery.of(context).size.width * 0.8,
+                  MediaQuery.of(context).size.width * 0.15,
+                )),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/edit_name_ui.dart
+++ lib/screens/edit_name_ui.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EditnameUi extends StatefulWidget {
   const EditnameUi({super.key});
@@ -14,13 +18,69 @@ class _EditnameUiState extends State<EditnameUi> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+          "แก้ไขชื่อ",
+          style: GoogleFonts.kanit(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
           },
           icon: Icon(Icons.arrow_back_ios),
         ),
+        centerTitle: true,
         backgroundColor: Colors.lightGreen,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.10,
+              ),
+              Icon(
+                FontAwesomeIcons.person,
+                size: MediaQuery.of(context).size.width * 0.35,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.10,
+              ),
+              Text(
+                'ป้อมชื่อ',
+                style: GoogleFonts.kanit(
+                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.width * 0.03),
+              Padding(
+                padding: const EdgeInsets.only(left: 40, right: 40),
+                child: TextField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.03,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Done"),
+                style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                  MediaQuery.of(context).size.width * 0.8,
+                  MediaQuery.of(context).size.width * 0.15,
+                )),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
